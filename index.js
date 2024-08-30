4d53a309c10191a137c6faa20c8ea7673deee4a2
nou-n <me@noun.kr>
2024-08-30 18:35:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const { search } = require("./wrtn");
+const { wrtn } = require("./wrtn");
 
 async function main() {
-    const message = await search("대한민국 수도에 대해 알려줘");
-    console.log(message.message);
+    const client = new wrtn();
+    const connect = await client.connect();
+    if(connect.success) {
+        client.onMessage((message) => {
+            const response = message.response;
+            switch (message.type) {
+                case "end":
+                    console.log(response.message.content);
+                    break;
+                case "generateStep":
+                    if(response.message) console.log(`[${Date().substring(0, 24)}] ${response.message}`);
+                    break;
+            }
+        });
+        client.sendMessage("golang에 대해서 알려줘.") ? console.log("전송에 성공했어요.") : console.log("전송에 실패했어요.");
+    }
 }
-
 main();
